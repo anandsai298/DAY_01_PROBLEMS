a82692b41430843eb80e040b138e2cd7bed0f105
anand <anandsai298@gmail.com>
2023-05-11 22:33:10 +0530

    [sai] Add: Compute Average of numbers. Generate 5 random numbers between 10 to 50  and compute the average of these numbers . Print the numbers generated in one line and in next line print the average

---
+++ DAY_01_PROBLEMS/DAY_01_PROBLEMS/Avg_Random_Num.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_01_PROBLEMS
+{
+    public class Avg_Random_Num
+    {
+        public void Random_Avg()
+        {
+           
+            int[] Rand=new int[5];
+            for(int i=0; i<5; i++)
+            {
+                Random random = new Random();
+                Rand[i] = random.Next(10, 51);
+            }
+            
+            double sum = 0;
+            foreach(int j in Rand)
+            {
+                sum += j;
+            }
+            double avg = sum / 5;
+            Console.WriteLine("random numbers (10-51) :"+string.Join(",",Rand)+"\n"+"Average of random numbers:"+avg);
+        }
+    }
+}
--- DAY_01_PROBLEMS/DAY_01_PROBLEMS/Program.cs
+++ DAY_01_PROBLEMS/DAY_01_PROBLEMS/Program.cs
@@ -11,7 +11,7 @@ class problem
         while(flag)
         {
             Console.WriteLine("Select option to display");
-            Console.WriteLine("1.ListOfFriendsName\n2.Math1\n3.Perimetre_Area_Circle\n4.String_Num\n5.DateTime_ExecutionTime");
+            Console.WriteLine("1.ListOfFriendsName\n2.Math1\n3.Perimetre_Area_Circle\n4.String_Num\n5.DateTime_ExecutionTime\n6.Avg_Random_Num");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -38,6 +38,10 @@ class problem
                 case 5:
                     DateTime_ExecutionTime dt= new DateTime_ExecutionTime();
                     dt.Date_Execution_Time();
+                    break;
+                case 6:
+                    Avg_Random_Num arn=new Avg_Random_Num();
+                    arn.Random_Avg();
                     break;
 
             }
